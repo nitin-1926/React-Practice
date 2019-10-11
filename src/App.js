5434644991e49840d3109ebf9030a21002fec187
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-11 14:13:39 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -15,3 +15,64 @@ class App extends React.Component{
   }
 }
 export default App;
+// import React from 'react';
+// import './App.css';
+// import Input from "./components/input";
+// import BUTTON from "./components/button";
+
+// let z=122
+// let p="AAA"
+// let fgt = "CLICK ME DUDE"
+// class App extends React.Component{
+// a=0;
+//   state={
+//     val:"",
+//     todo:[]
+//   }
+//   addList=(e)=>{
+//     const {val,todo} = this.state;
+//     todo.push(val);
+//     console.log(todo);
+//     this.setState({todo:todo,val:" "});
+//   }
+//   handleInput=(e)=>{
+//     const value = e.target.value;
+//     this.setState({val:value});
+//   }
+//   deleteTask=(e)=>{
+//     const {todo} = this.state;
+//     const index = e.target.id;
+//     todo.splice(index,1);
+//     this.setState({todo:todo});
+//   }
+//   render(){
+//     const {val:a,todo} = this.state;
+//     return (
+//       <div className={p}>
+//         <div id={z}><button>{fgt}</button>
+//         <Input placeholder="Write anything you want" /></div>
+//         <div>
+//           <Input value = {a} onChange = {this.handleInput} />
+//           <BUTTON onClick = {this.addList} value = "ADD" />
+//         </div>
+//           <div>
+//             <ul>
+//             {
+//               todo.map((item,i)=>
+//               {
+//                 return(
+//                   <div key={i}>
+//                     <li>{item}</li>
+//                     <BUTTON id = {i} onClick = {this.deleteTask} value="Delete(X)" />
+//                   </div>
+//                 )
+//               })
+//             }
+//             </ul>
+//           </div>
+//         </div>
+//     );
+//   }
+// }
+
+// export default App;
