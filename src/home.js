52466fce984dc2c70163d9ec59c1c44c934dbdff
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-12 09:28:58 +0530

    props

--- src/home.js
+++ src/home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 
-export default function(){
+export default function(props){
+    {console.log(props)}
     return(
         <div>
             HOME AI yeh
